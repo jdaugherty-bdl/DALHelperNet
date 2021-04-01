69dc8bb0738070da02c3318bcb396f1ecc2b4dcb
John Daugherty <jdaugherty0713@gmail.com>
2021-04-01 18:13:36 -0400

    added a standard user db model template

---
+++ DALHelperNet/Models/DALUserDbModel.cs
@@ -0,0 +1,24 @@
+﻿using DALHelperNet.Interfaces.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALHelperNet.Models
+{
+    public class DALUserDbModel : DALBaseModel
+    {
+        [DALResolvable]
+        public int UserId { get; set; }
+        [DALResolvable]
+        public string UserEmail { get; set; }
+        [DALResolvable]
+        public string UserName { get; set; }
+
+        public DALUserDbModel() : base() { }
+        private static string ThisTypeName => typeof(DALUserDbModel).Name;
+        public DALUserDbModel(DataRow TrackerRow, string AlternateTableName = null) : base(TrackerRow, AlternateTableName ?? ThisTypeName) { }
+    }
+}
