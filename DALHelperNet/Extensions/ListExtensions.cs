3f59c09f30f827ac1631a33c58fbce9edd4987e5
John Daugherty <jdaugherty0713@gmail.com>
2021-04-01 18:28:33 -0400

    add list extension writetodatabase()

---
+++ DALHelperNet/Extensions/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using DALHelperNet.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALHelperNet.Extensions
+{
+    public static class ListExtensions
+    {
+        public static int WriteToDatabase<T>(this IEnumerable<T> DbModelData, Enum ConnectionStringType) where T : DALBaseModel
+        {
+            return DALHelper.BulkTableWrite<T>(ConnectionStringType, DbModelData);
+        }
+
+        public static int WriteToDatabase<T>(this IEnumerable<T> DbModelData, MySqlConnection ExistingConnection, MySqlTransaction SqlTransaction = null) where T : DALBaseModel
+        {
+            return DALHelper.BulkTableWrite<T>(ExistingConnection, DbModelData, SqlTransaction: SqlTransaction);
+        }
+    }
+}
